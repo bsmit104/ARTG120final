94f3e849fe35c8f4daf193e94bc760fcf828c348
r2 <jbrayden35@gmail.com>
2023-05-25 14:06:59 -0700

    removed boundary debug

--- game.js
+++ game.js
@@ -26,7 +26,7 @@ var config = {
                 x: 0,
                 y: 0
             },
-            debug: true
+            debug: false
         }
     },
     scene: [Start, Intro, Title, Level1, Cut0, Level2, Cut1, Level3, Cut2, Level4, Cut3]
