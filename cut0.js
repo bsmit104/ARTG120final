e7582b2177f18dfc5094ba0dada7067618da1375
r2 <jbrayden35@gmail.com>
2023-06-03 22:09:37 -0700

    done

--- cut0.js
+++ cut0.js
@@ -5,36 +5,66 @@ class Cut0 extends Phaser.Scene {
     preload() {
         this.load.path = "./assets/";
         this.load.video("fishvid", "fish.mp4");
+        this.load.audio("boom", "Nuke.mp3");
         this.load.audio('fishMusic', 'fishjams.mp3');
     }
     create() {
         this.fishMusic = this.sound.add('fishMusic');
         this.fishMusic.play();
         this.fishMusic.loop = true;
+
+        this.time.addEvent({
+            delay: 10000, 
+            loop:false,
+            callback: () => {
+                this.boom = this.sound.add('boom');
+                    // Play the sound
+                this.boom.play();
+                this.boom.loop = false;
+            }
+        })
+        // this.time.addEvent({
+        //     delay: 10000,
+        //     callback: this.playSound,
+        //     callbackScope: this
+        // });
+
+        // function playSound() {
+        //     // Create the sound object
+        //     this.boom = this.sound.add('boom');
+        //     // Play the sound
+        //     this.boom.play();
+        //     this.boom.loop = false;
+        // }
+
+        // this.boom = this.sound.add('boom');
+
+        // this.boom.play();
+        // this.boom.loop = false;
         //this.add.text(300, 300, "cut 0");
 
         const video = this.add.video(400, 300, "fishvid");
         video.play();
 
         video.on('complete', () => {
-        this.textObject8 = this.add.text(
-            700, //x
-            500,//y
-            "tap", //text
-            {
-                font: "30px Impact",
-                color: "#FFFFFF",
-                align: "center"
-            } //style
-        );
-        this.tweens.add({
-            targets: this.textObject8,
-            alpha:0,
-            duration: 2000,
-            repeat: -1,
+            this.textObject8 = this.add.text(
+                700, //x
+                500,//y
+                "tap", //text
+                {
+                    font: "30px Impact",
+                    color: "#FFFFFF",
+                    align: "center"
+                } //style
+            );
+            this.tweens.add({
+                targets: this.textObject8,
+                alpha: 0,
+                duration: 2000,
+                repeat: -1,
+            });
+            this.fishMusic.stop();
+            this.input.on('pointerdown', () => this.scene.start('level2'));
         });
-        this.fishMusic.stop();
-        this.input.on('pointerdown', () => this.scene.start('level2'));
-    });
     }
 }
--- cut1.js
+++ cut1.js
@@ -4,6 +4,8 @@ class Cut1 extends Phaser.Scene {
     }
     preload() {
         this.load.path = "./assets/";
+        this.load.audio('beat', 'heartbeat.mp3');
+        this.load.audio('ahh', 'Rat_death.mp3');
         this.load.video("slimerat", "slime_to_rat.mp4");
         // this.load.spritesheet('pos', 'slimepossess.png', {
         //     frameWidth: 50,
@@ -13,6 +15,21 @@ class Cut1 extends Phaser.Scene {
         // }); //29
     }
     create() {
+
+        this.beat = this.sound.add('beat');
+        this.beat.play();
+        this.beat.loop = true;
+
+        this.time.addEvent({
+            delay: 1000, 
+            loop:false,
+            callback: () => {
+                this.ahh = this.sound.add('ahh');
+                    // Play the sound
+                this.ahh.play();
+                this.ahh.loop = true;
+            }
+        })
 
         const video = this.add.video(400, 300, "slimerat");
         video.play();
@@ -34,6 +51,8 @@ class Cut1 extends Phaser.Scene {
             duration: 2000,
             repeat: -1,
         });
+        this.beat.stop();
+        this.ahh.stop();
         this.input.on('pointerdown', () => this.scene.start('level3'));
     });
 
